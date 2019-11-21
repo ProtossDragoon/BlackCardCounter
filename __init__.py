02fc4db160b22064a97a5c62c63f1e216e33427a
hhhhhhhhhhhhhhhhho <man2719@naver.com>
2019-11-21 21:05:01 +0900

    Main UI

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import *
-
-app = QApplication(sys.argv)
-
-app.s
-label = QLabel("Hello PyQt")
-label.show()
-app.exec_()
---
+++ docs/study/README.md
@@ -0,0 +1,11 @@
+
+<br>
+
+python 프로젝트 파일 구성하기 : https://python-guide-kr.readthedocs.io/ko/latest/writing/structure.html#test-suite
+> 
+
+
+
+<br>
+
+
